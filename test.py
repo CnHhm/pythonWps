38a33258c64601e426b3a2019e1a9e6738f91806
hhm <hm.hu@outlook.com>
2020-12-17 17:29:47 +0800

    read the file name

---
+++ test.py
@@ -0,0 +1,9 @@
+import os
+import tkinter
+
+#!/usr/bin/python3
+ 
+import tkinter
+top = tkinter.Tk()
+# 进入消息循环
+top.mainloop()
--- win32wps.py
+++ win32wps.py
@@ -8,21 +8,25 @@
 import win32com.client
 
 # 打开wps的表格et
-et = win32com.client.Dispatch("Excel.Application")
+# et = win32com.client.Dispatch("Excel.Application")
+et = win32com.client.Dispatch("Word.Application")
 et.Visible = True # 确定ET是否可见
-file = r'e:\12-15lj\中文路径测试.xls'
+fileExcel = r'D:\project\pythonWps\test.xls'
+fileWord = r'D:\project\pythonWps\test.doc'
 # 打开wps表格文件
-wb = et.Workbooks.Open(file)
-# # 新建一个wps工作簿
-# # wb = et.Workbooks.Add()
-# 打开wps表格文件中的第一张表
-sht = wb.Worksheets(1)
-# 修改单元格的内容
-string = "福建省游泳馆"
-sht.Cells(2, 5).Value = '单位：'+string+'            2020 年 12月 16日'
-# 保存文件 另存为命令为workbook.SaveAs()
-wb.Save()
-# 关闭文件
-wb.Close()
-# 退出程序
-et.Quit()
+# wb = et.Workbooks.Open(fileWord)
+wb = et.Documents.Open(fileWord)
+# newdoc = et.Documents.Add()
+# # # 新建一个wps工作簿
+# # # wb = et.Workbooks.Add()
+# # 打开wps表格文件中的第一张表
+# sht = wb.Worksheets(1)
+# # 修改单元格的内容
+# string = "福建省游泳馆"
+# sht.Cells(2, 5).Value = '单位：'+string+'            2020 年 12月 16日'
+# # 保存文件 另存为命令为workbook.SaveAs()
+# wb.Save()
+# # 关闭文件
+# wb.Close()
+# # 退出程序
+# et.Quit()
